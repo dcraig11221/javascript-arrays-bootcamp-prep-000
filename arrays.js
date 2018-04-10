95eb52719b1eb0afba8a82c31ebbb7946d9d113e
Douglas Craig <dcraig112@gmail.com>
2018-04-10 16:32:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,3 +25,8 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]
 }
+
+function removeElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
